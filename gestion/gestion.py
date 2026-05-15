42ed039b3745a4ac4702a60278fbb97da25687d4
R00KIE9111 <leonardo.concha06@inacapmail.cl>
2026-05-15 05:35:07 -0400

    1.0.02

--- gestion/gestion.py
+++ gestion/gestion.py
@@ -6,10 +6,10 @@ from datetime import datetime
 # --- Conexión a la BD ---
 def get_connection():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="tu_password",
-        database="tu_base"
+        host="database-1.c7saowikmas3.us-east-1.rds.amazonaws.com",
+        user="admin",
+        password="Inacap.2026",
+        database="gestorecommerce"
     )
 
 # --- Autenticación ---
