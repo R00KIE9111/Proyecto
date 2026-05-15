5e669d9e8f821a4e7d5c5ceb33b94d9fa27f9217
R00KIE9111 <leonardo.concha06@inacapmail.cl>
2026-05-15 05:29:46 -0400

    1.0.1

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,6 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
-from .routes import init_routes
+from . import routes
 
 def create_app():
-    app = Flask(__name__)
-    app.secret_key = "clave-secreta"  # cámbiala en producción
-
-    # Protección CSRF
-    CSRFProtect(app)
-
-    # Registrar rutas
-    init_routes(app)
-
+    app = routes.app
     return app
